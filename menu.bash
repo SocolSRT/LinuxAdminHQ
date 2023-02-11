3b99c51e7949e137461a87c047c0467162f2cdfa
Socol <55624740+SocolSRT@users.noreply.github.com>
2023-02-11 16:48:28 +0200

    Update menu.bash

--- menu.bash
+++ menu.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 clear
 echo "#########################################
 # Nginx Web Server Management Menu #
@@ -14,17 +13,15 @@ echo "#########################################
 7. Add SSL certificate
 8. Connecting SSL
 9. Exit
-"
 
+"
 while true; do
   read -p "Enter your choice [1-9]: " choice
-
   case $choice in
     1)
       # Install Nginx
       echo "Available Nginx versions: 1.18.0, 1.19.5, 1.20.2"
       read -p "Enter the Nginx version you want to install (e.g. 1.20.2): " nginx_version
-
       echo "Installing Nginx $nginx_version..."
       sudo apt-get update
       sudo apt-get install nginx=$nginx_version-*
@@ -34,7 +31,6 @@ while true; do
       # Install PHP
       echo "Available PHP versions: 7.2, 7.4, 8.0"
       read -p "Enter the PHP version you want to install (e.g. 7.4): " php_version
-
       echo "Installing PHP $php_version..."
       sudo apt-get install php$php_version-fpm php$php_version-mysql php$php_version-gd php$php_version-curl php$php_version-json php$php_version-mbstring php$php_version-xml
       echo "PHP $php_version has been installed."
@@ -42,7 +38,6 @@ while true; do
     3)
       # Add a Domain
       read -p "Enter the domain name: " domain
-
       # Create a folder for the domain
       sudo mkdir -p /var/www/$domain
       echo "Folder for $domain has been created. (/var/www/$domain)"
@@ -51,14 +46,11 @@ while true; do
     listen 80;
     server_name $domain;
     root /var/www/$domain;
-
     # logging
     access_log /var/log/nginx/$domain.access.log;
     error_log /var/log/nginx/$domain.error.log warn;
-
     # index
     index index.php index.html index.htm default.php default.htm default.html;
-
     # handle .php
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
@@ -70,7 +62,6 @@ sudo service nginx restart
     4)
       # Remove a Domain
       read -p "Enter the domain name: " domain
-
       # Remove the folder for the domain
       echo "Removing the folder for $domain..."
       sudo rm -rf /var/www/$domain
@@ -86,7 +77,6 @@ sudo service nginx restart
       read -p "Enter the domain name: " domain
       read -p "Enter the IP: " IPdomain
       echo "Adding reverse proxy for $domain..."
-
       # Add the following code to the Nginx configuration:
 	  sudo rm /etc/nginx/sites-enabled/$domain
       echo -n "server {
@@ -96,9 +86,7 @@ sudo service nginx restart
         proxy_pass         http://$IPdomain:80;
     }
 }" | sudo tee -a /etc/nginx/sites-enabled/$domain
-
       # Save and exit the configuration file
-
       # Restart Nginx to apply changes
       sudo service nginx restart
       echo "Reverse proxy has been added for $domain."
@@ -106,11 +94,8 @@ sudo service nginx restart
     6)
       # Remove Reverse Proxy
       read -p "Enter the domain name: " domain
-
 	  sudo rm /etc/nginx/sites-enabled/$domain
-
       # Save and exit the configuration file
-
       # Restart Nginx to apply changes
       sudo service nginx restart
       echo "Reverse proxy has been removed for $domain."
@@ -133,19 +118,15 @@ sudo service nginx restart
     listen 443 ssl;
     server_name $domain;
     root /var/www/$domain;
-
     ssl_certificate /etc/nginx/ssl/$domain.crt;
     ssl_certificate_key /etc/nginx/ssl/$domain.key;
     ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
     ssl_ciphers HIGH:!aNULL:!MD5;
-
     # logging
     access_log /var/log/nginx/$domain.access.log;
     error_log /var/log/nginx/$domain.error.log warn;
-
     # index
     index index.php index.html index.htm default.php default.htm default.html;
-
     # handle .php
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
